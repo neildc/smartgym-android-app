9a207c89c3576d8ea8a5b0216f7856bbba9934eb
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2016-11-13 19:49:58 +1100

    Added in the keys and stuff

---
+++ app/src/main/java/com/example/doge/smartgym3/util/ServiceGenerator.java
@@ -0,0 +1,94 @@
+package com.example.doge.smartgym3.util;
+
+import com.example.doge.smartgym3.BuildConfig;
+import com.facebook.AccessToken;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+/**
+ * Created by neil on 15/10/16.
+ */
+public class ServiceGenerator {
+    private static final String API_BASE_URL = BuildConfig.API_BASE_URL;
+
+
+    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+    private static Retrofit.Builder builder =
+            new Retrofit.Builder()
+                    .baseUrl(API_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create());
+
+    static <S> S createService(Class<S> serviceClass) {
+        Retrofit retrofit = builder.client(httpClient.build()).build();
+        return retrofit.create(serviceClass);
+    }
+
+    /**
+     * Converting facebook access token to timetablr token
+     */
+    public static <S> S createJSONService(Class<S> serviceClass, AccessToken token) {
+        if (token != null) {
+            httpClient.addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Interceptor.Chain chain) throws IOException {
+                    Request original = chain.request();
+
+                    Request.Builder requestBuilder = original.newBuilder()
+                            .header("Accept", "application/json")
+                            .method(original.method(), original.body());
+
+                    Request request = requestBuilder.build();
+                    return chain.proceed(request);
+                }
+            });
+        }
+
+        OkHttpClient client = httpClient.build();
+        Retrofit retrofit = builder.client(client).build();
+        return retrofit.create(serviceClass);
+    }
+
+    /**
+     * Make calls with the converted authToken in the header
+     * @param serviceClass Any of the services in the server folder
+     * @param authToken authtoken
+     * @param <S>
+     * @return
+     */
+    public static <S> S createService(Class<S> serviceClass, final String authToken) {
+        if (authToken != null) {
+            httpClient.addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Interceptor.Chain chain) throws IOException {
+                    Request original = chain.request();
+
+                    // Request customization: add request headers
+                    Request.Builder requestBuilder = original.newBuilder()
+                            .header("Authorization", "BEARER " + authToken)
+                            .method(original.method(), original.body());
+
+                    Request request = requestBuilder.build();
+                    return chain.proceed(request);
+                }
+            });
+        }
+
+        OkHttpClient client = httpClient.build();
+        Retrofit retrofit = builder.client(client).build();
+        return retrofit.create(serviceClass);
+    }
+
+}
