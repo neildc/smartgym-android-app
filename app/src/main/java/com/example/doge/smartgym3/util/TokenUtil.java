9c9e75098fdddaf3ef00b6cb7bb0b0be27748a46
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2016-11-16 17:07:19 +1100

    fixed TokenUtil

--- app/src/main/java/com/example/doge/smartgym3/util/TokenUtil.java
+++ app/src/main/java/com/example/doge/smartgym3/util/TokenUtil.java
@@ -3,7 +3,9 @@ package com.example.doge.smartgym3.util;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
+import com.example.doge.smartgym3.BuildConfig;
 import com.example.doge.smartgym3.R;
 import com.example.doge.smartgym3.server.LoginService;
 import com.facebook.AccessToken;
@@ -25,30 +27,28 @@ public class TokenUtil {
         return accessToken != null;
     }
 
-    void getTimetablrAccessTokenFromFacebookAccessToken(final Context context, AccessToken accessToken) {
+    public static void getServerAccessTokenFromFacebookAccessToken(final Context context, AccessToken accessToken) {
         JsonObject resp = null;
         try {
-            resp = this.convertFacebookTokenToLocal(accessToken).execute().body();
+            resp = convertFacebookTokenToLocal(accessToken).execute().body();
             saveAccessTokenToSharedPreferences(context,resp.get("access_token").getAsString());
         } catch (IOException e) {
+
+            Log.wtf("ERROR", "Network error: Failed to convert accesstoken");
+            Log.wtf("ERROR", e.toString());
+//            throw new IOException("Failed to convert accessTOken");
 
         }
 
 
     }
 
-    private void saveAccessTokenToSharedPreferences(Context context, String accessToken) {
+    private static void saveAccessTokenToSharedPreferences(Context context, String accessToken) {
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
         String timetablrTokenKey =
                 context.getResources().getString(R.string.internal_pref_smartgym_token_key);
         sharedPref.edit().putString(timetablrTokenKey, accessToken).apply();
 
-        // TODO(doyle): Remove on confirm replacement code works
-        /*
-            String sharedPrefFile = context.getResources().getString(R.string.preference_file_key);
-            SharedPreferences.Editor editor = context.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE).edit();
-            editor.putString("timetablrAccessToken", accessToken);
-        */
     }
 
     public static String getAccessTokenFromSharedPreferences(Context context) {
@@ -59,25 +59,15 @@ public class TokenUtil {
 
         return accessToken;
 
-        // TODO(doyle): Remove on confirm replacement code works
-        /*
-        String sharedPrefFile = context.getResources().getString(R.string.preference_file_key);
-        SharedPreferences prefs = context.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE);
-        return (prefs.getString("timetablrAccessToken", null));//null is the default value.
-        String restoredText = prefs.getString("text", null);
-        if (restoredText != null) {
-        }
-        return null;
-        */
     }
 
-    private Call<JsonObject> convertFacebookTokenToLocal(AccessToken accessToken) {
+    private static Call<JsonObject> convertFacebookTokenToLocal(AccessToken accessToken) {
 
         // If the access token is available already assign it.
         accessToken = AccessToken.getCurrentAccessToken();
 
-        String clientId = BuildConfig.TIMETABLR_KEY;
-        String clientSecret = BuildConfig.TIMETABLR_SECRET;
+        String clientId = BuildConfig.API_KEY;
+        String clientSecret = BuildConfig.API_SECRET;
         String backend = "facebook";
 
         LoginService loginService =
