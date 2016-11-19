ed28dc604f2baf2f862b840ef9d32dea5cdd9861
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2016-11-20 08:13:59 +1100

    Redid exercise details

--- app/src/main/java/com/example/doge/smartgym3/ExerciseDetailActivity.java
+++ app/src/main/java/com/example/doge/smartgym3/ExerciseDetailActivity.java
@@ -2,6 +2,8 @@ package com.example.doge.smartgym3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,6 +13,11 @@ import com.example.doge.smartgym3.util.ServiceGenerator;
 import com.example.doge.smartgym3.util.TokenUtil;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -26,16 +33,43 @@ public class ExerciseDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_detail);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_exercise_detail);
+        this.setSupportActionBar(myToolbar);
+        this.getSupportActionBar().setTitle("Exercise Details");
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         exercise_id = getIntent().getIntExtra("exercise_id", 0);
 
-        madText = (TextView) findViewById(R.id.detailText);
+//        madText = (TextView) findViewById(R.id.detailText);
         this.exerciseService = ServiceGenerator.createService(
                 ExerciseService.class,
                 TokenUtil.getAccessTokenFromSharedPreferences( this )
         );
 
         getData(exercise_id);
+    }
+
+    public void setTextForAllTextViews(String exercise,
+                          String date,
+                          String time,
+                          int weight,
+                          int resttime,
+                          int sets,
+                          int reps ) {
+
+        setTextOfTextView(R.id.exercise_detail_exercise_type, exercise);
+        setTextOfTextView(R.id.exercise_detail_date, date);
+        setTextOfTextView(R.id.exercise_detail_time, time);
+        setTextOfTextView(R.id.exercise_detail_weight, Integer.toString(weight));
+        setTextOfTextView(R.id.exercise_detail_rest, Integer.toString(resttime));
+        setTextOfTextView(R.id.exercise_detail_sets, Integer.toString(sets));
+        setTextOfTextView(R.id.exercise_detail_reps, Integer.toString(reps));
+
+    }
+
+    private void setTextOfTextView(int id, String text) {
+        TextView tv = (TextView)findViewById(id);
+        tv.setText(text);
     }
 
     private void getData(int exerciseId) {
@@ -47,7 +81,50 @@ public class ExerciseDetailActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
                 if (response.isSuccessful()) {
-                    madText.setText(response.body().toString());
+//                    madText.setText(response.body().toString());
+
+                    /*
+                    {
+                        "user": 2,
+                        "id": 63,
+                        "created": "2016-11-19T10:54:30.456878Z",
+                        "exercise_type": "Bench Press",
+                        "targetReps": 5,
+                        "targetSets": 5,
+                        "restTime": 5,
+                        "station": 1,
+                        "weight": 33,
+                        "sets": []
+                    }
+                     */
+
+                    JsonObject data = response.body().getAsJsonObject();
+                    String datetimeString = data.get("created").getAsString();
+
+                    DateFormat iso8601Format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+                    Date date = new Date();
+
+                    try {
+                        date = iso8601Format.parse(datetimeString);
+                    } catch (ParseException e) {
+                        Log.e("EXERCISE_DETAIL", e.toString());
+                    }
+
+                    SimpleDateFormat dateFormatter = new SimpleDateFormat("EEEE, d/M/y");
+                    SimpleDateFormat timeFormatter = new SimpleDateFormat("hh:mm a");
+
+                    String dateString = dateFormatter.format(date);
+                    String timeString = timeFormatter.format(date);
+
+                    setTextForAllTextViews(
+                            data.get("exercise_type").getAsString()+" Exercise",
+                            dateString,
+                            timeString,
+                            data.get("weight").getAsInt(),
+                            data.get("restTime").getAsInt(),
+                            data.get("targetSets").getAsInt(),
+                            data.get("targetReps").getAsInt()
+                    );
                 }
             }
 
